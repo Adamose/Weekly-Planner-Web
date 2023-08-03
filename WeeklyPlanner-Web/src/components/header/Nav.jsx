32714075df1ca411d05426557a60d510788d878e
Adam <adam.esanhaji@outlook.com>
2023-08-03 13:20:18 -0400

    Added boolean state to nav buttons

--- WeeklyPlanner-Web/src/components/header/Nav.jsx
+++ WeeklyPlanner-Web/src/components/header/Nav.jsx
@@ -1,15 +1,29 @@
 import { Context } from "../../GlobalContext.jsx";
 import HeaderButton from "./HeaderButton.jsx";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function Nav() {
 
     const { isPlannerOpen, setIsPlannerOpen } = useContext(Context);
+    const [isAnimating, setIsAnimating] = useState(false);                   //Boolean to prevent button click during page switch
+
+    //Nav buttons click handler
+    const handleClick = (boolean) => {
+        if (!isAnimating) {
+            setIsPlannerOpen(boolean);
+            setIsAnimating(true);
+
+            //Waiting for animation to be done before setting boolean to false
+            setTimeout(() => {
+                setIsAnimating(false);
+            }, 700);
+        }
+    };
 
     return (
         <nav className="header-nav">
-            <button className={"nav-button" + (isPlannerOpen ? " nav-button-selected" : "")} onClick={() => setIsPlannerOpen(true)}>Planner</button>
-            <button className={"nav-button" + (isPlannerOpen ? "" : " nav-button-selected")} onClick={() => setIsPlannerOpen(false)}>Tasks</button>
+            <button className={"nav-button" + (isPlannerOpen ? " nav-button-selected" : "")} onClick={() => handleClick(true)}>Planner</button>
+            <button className={"nav-button" + (isPlannerOpen ? "" : " nav-button-selected")} onClick={() => handleClick(false)}>Tasks</button>
             <HeaderButton />
         </nav>
     );
